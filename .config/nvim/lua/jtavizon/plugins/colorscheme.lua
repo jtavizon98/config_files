6e4dbc1ad68ff60ccf7fbefcb48074522fa1ad86
jtavizon98 <67345033+jtavizon98@users.noreply.github.com>
2024-03-26 19:37:50 +0100

    Plugin improvements

--- .config/nvim/lua/jtavizon/plugins/colorscheme.lua
+++ .config/nvim/lua/jtavizon/plugins/colorscheme.lua
@@ -1,10 +1,30 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = { flavour = mocha },
-    config = function()
-        -- load colorscheme
-        vim.cmd([[colorscheme catppuccin]])
-    end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {
+		flavour = "mocha",
+		background = {
+			light = "latte",
+			dark = "mocha",
+		},
+		integrations = {
+			alpha = true,
+			cmp = true,
+			indent_blankline = {
+				enabled = true,
+				scope_color = "",
+				colored_indent_levels = false,
+			},
+			nvimtree = true,
+			treesitter = true,
+			telescope = {
+				enabled = true,
+			},
+		},
+	},
+	config = function()
+		-- load colorscheme
+		vim.cmd([[colorscheme catppuccin]])
+	end,
 }
