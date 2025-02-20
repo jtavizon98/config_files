899837b720423441b8cc377ed1933764e8d5b8b2
jtavizon98 <jairodaniel2@gmail.com>
2025-02-20 17:46:00 +0100

    Random changes

---
+++ .config/nvim/lua/jtavizon/plugins/colorscheme.lua
@@ -0,0 +1,30 @@
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {
+		flavour = "latte",
+		background = {
+			light = "latte",
+			dark = "mocha",
+		},
+		integrations = {
+			alpha = true,
+			cmp = true,
+			indent_blankline = {
+				enabled = true,
+				scope_color = "",
+				colored_indent_levels = false,
+			},
+			nvimtree = true,
+			treesitter = true,
+			telescope = {
+				enabled = true,
+			},
+		},
+	},
+	config = function()
+		-- load colorscheme
+		vim.cmd([[colorscheme catppuccin]])
+	end,
+}
