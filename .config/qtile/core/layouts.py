119fa2ddc49248f9386d0af4712ec76ab39a5dc7
jtavizon98 <67345033+jtavizon98@users.noreply.github.com>
2026-01-13 18:33:40 +0100

    fixes for deprecated stuff

--- .config/qtile/core/layouts.py
+++ .config/qtile/core/layouts.py
@@ -1,7 +1,7 @@
 from libqtile import layout
 from libqtile.config import Match
 
-from colors import catppuccin
+from core.colors import catppuccin
 
 layout_theme = {
     "border_width": 2,
@@ -13,34 +13,14 @@ layout_theme = {
 
 def init_layouts():
     layouts = [
-        # layout.Zoomy(**layout_theme),
         layout.MonadTall(**layout_theme),
         layout.Max(**layout_theme),
-        # layout.Stack(stacks=2, **layout_theme),
-        # layout.RatioTile(
-        #     **layout_theme,
-        #     clockwise=True,
-        #     new_client_position='bottom',
-        #     ratio = 0.5
-        # ),
-        # layout.Spiral(
-        #     **layout_theme,
-        #     clockwise=True,
-        #     new_client_position='after_current',
-        #     ratio = 0.5
-        # ),
         layout.Floating(**layout_theme),
     ]
     floating_layout = layout.Floating(
         float_rules=[
-            # Run the utility of `xprop` to see the wm class and name of an X client.
             *layout.Floating.default_float_rules,
-            Match(wm_class="confirmreset"),  # gitk
-            Match(wm_class="makebranch"),  # gitk
-            Match(wm_class="maketag"),  # gitk
-            Match(wm_class="ssh-askpass"),  # ssh-askpass
-            Match(title="branchdialog"),  # gitk
-            Match(title="pinentry"),  # GPG key password entry
+            Match(wm_class="zen", title=""),
         ]
     )
     return layouts, floating_layout
